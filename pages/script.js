98b7ff1008dd70523ea04971e7b498b47c4ca618
amaaaaaaaaaan <aman.khaan657@gmail.com>
2024-07-30 23:30:16 +0530

    navbar_done

---
+++ pages/script.js
@@ -0,0 +1,6 @@
+let searchBar = document.getElementById("searchbar")
+let searchClickFunc = () => {
+  
+    searchBar.style.border="1px solid gray"
+};
+searchBar.addEventListener("click", searchClickFunc);
